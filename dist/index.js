624ba7a370b880d31202e32a355a72f17fe2fda9
Maciek Siemczyk <msiemczyk@live.ca>
2021-03-22 09:39:12 -0400

    Update index.js
    Opened up the tag regex.

--- dist/index.js
+++ dist/index.js
@@ -7058,7 +7058,7 @@ function getConfig(path) {
   return DEFAULT_CONFIG;
 }
 
-const TAG_REGEX = /^v?(\d+).(\d+).(\d+)$/i;
+const TAG_REGEX = /^v?(\d+).(\d+).(\d+).*$/i;
 
 function validateTag(tag) {
   return TAG_REGEX.test(tag);
